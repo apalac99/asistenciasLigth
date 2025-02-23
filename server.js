fb472150cb267d4bd2a3169425f66683d5f42063
Alejandro Palacios <apalac99@gmail.com>
2025-02-23 09:06:10 -0500

    2

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Student = require('./models/Student');
+const cors = require('cors'); // Importar el paquete cors
+const Student = require('./models/Students');
 
 const app = express();
+
+// Habilitar CORS para todas las solicitudes
+app.use(cors());
+
+// Middleware para parsear JSON
 app.use(express.json());
 
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
